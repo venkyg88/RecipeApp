fc3e598f397af29d8f9779486bd7539638d3f598
Venkat Gonuguntala <venkyg88@gmail.com>
2016-08-01 17:01:15 -0400

    Initial commit till recycler view

---
+++ app/src/main/java/fragments/venkat/com/myapplication/ListFragment.java
@@ -0,0 +1,21 @@
+package fragments.venkat.com.myapplication;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by venkatgonuguntala on 8/1/16.
+ */
+
+public class ListFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/fragments/venkat/com/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package fragments.venkat.com.myapplication;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListFragment listFragment = new ListFragment();
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.placeHolder, listFragment);
+        fragmentTransaction.commit();
+    }
+}
