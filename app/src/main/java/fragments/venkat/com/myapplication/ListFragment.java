cbfda1d7fda5b4e7870ee7ef7acdd7389985f2c5
Venkat Gonuguntala <venkyg88@gmail.com>
2016-08-01 17:50:01 -0400

    adding the list view support for recipes

--- app/src/main/java/fragments/venkat/com/myapplication/ListFragment.java
+++ app/src/main/java/fragments/venkat/com/myapplication/ListFragment.java
@@ -3,6 +3,8 @@ package fragments.venkat.com.myapplication;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +18,13 @@ public class ListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        ListAdapter listAdapter = new ListAdapter();
+        recyclerView.setAdapter(listAdapter);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
         return view;
     }
 }
