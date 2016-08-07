42b9b2c4665c92ce07c1457b334f200ee4166b1a
Venkat Gonuguntala <venkyg88@gmail.com>
2016-08-07 10:41:41 -0400

    adding Dual Pane fragment to support tablets

---
+++ app/src/main/java/fragments/venkat/com/myapplication/DualPaneFragment.java
@@ -0,0 +1,21 @@
+package fragments.venkat.com.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by venkatgonuguntala on 8/7/16.
+ */
+
+public class DualPaneFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+    }
+}
